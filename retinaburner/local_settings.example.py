d4366b3a5da8dc52c5fb6247a34003489e9d38f0
Kaitlin Lee <katycorp@gmail.com>
2013-11-26 11:08:12 -0500

    first pages commit

--- retinaburner/local_settings.example.py
+++
@@ -1,7 +0,0 @@
-
-LOGGING_EMAIL = {'recipients': [ 'bla@bla.com',],
-                 'host': 'your.smtp.server',
-                 'port': 25,
-                 'username': 'someone',
-                 'password': 'somepword'
-                }
--- tsconfig.sql
+++
@@ -1,4 +0,0 @@
-create text search dictionary retinaburner ( template = simple, stopwords = retinaburner );
-create text search configuration retinaburner ( copy = simple );
-alter text search configuration retinaburner alter mapping for asciiword with retinaburner;
-commit;
