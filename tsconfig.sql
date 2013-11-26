904716aee87531eda369a17d8fb120a04e9f503c
Kaitlin Lee <katycorp@gmail.com>
2013-11-26 13:31:45 -0500

    config changes

--- tsconfig.sql
+++ tsconfig.sql
@@ -1,4 +1,4 @@
-create text search dictionary retinaburner ( template = simple, stopwords = retinaburner );
-create text search configuration retinaburner ( copy = simple );
-alter text search configuration retinaburner alter mapping for asciiword with retinaburner;
+create text search dictionary us_spending ( template = simple, stopwords = US_Spending );
+create text search configuration us_spending ( copy = simple );
+alter text search configuration us_spending alter mapping for asciiword with us_spending;
 commit;
